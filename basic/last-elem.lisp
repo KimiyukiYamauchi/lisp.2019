d68393c79c47e37610dcae0baec39a1acf12bf98
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2020-02-04 16:21:59 +0900

    get last element

--- basic/last-elem.lisp
+++ basic/last-elem.lisp
@@ -3,5 +3,5 @@
         (t (last-element2 x)) ))
 
 (defun last-element2 (x)
-  (cond  ((null (cdr x) (car x))
-          (t (last-element2 (cdr x)))) ))
+  (cond ((null (cdr x)) (car x))
+        (t (last-element2 (cdr x))) ))
